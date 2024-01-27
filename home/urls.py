44841e6b0ca4b7391e78938ff8f29d2d04078b92
iGagan <id4gsm@gmail.com>
2024-01-27 18:53:52 +0530

    "Added templates for home"

--- home/urls.py
+++ home/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
-
 from . import views
 
 urlpatterns = [
-    path("", views.index, name="index"),
+    # path("", views.index, name="index"),
+    path("", views.home_index, name="home_index"),
+    path("post/<int:pk>/", views.home_detail, name="home_detail"),
+    path("category/<category>/", views.home_category, name="home_category"),    
 ]
--- home/views.py
+++ home/views.py
@@ -1,5 +1,35 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from home.models import Post, Comment
+
 
 def index(request):
     return HttpResponse("You're at Home Page")
+
+
+def home_index(request):
+    posts = Post.objects.all().order_by("-created_on")
+    context = {
+        "posts": posts,
+    }
+    return render(request, "home/index.html", context)
+
+def home_category(request, category):
+    posts = Post.objects.filter(
+        categories__name__contains=category
+    ).order_by("-created_on")
+    context = {
+        "category": category,
+        "posts": posts,
+    }
+    return render(request, "home/category.html", context)
+
+def home_detail(request, pk):
+    post = Post.objects.get(pk=pk)
+    comments = Comment.objects.filter(post=post)
+    context = {
+        "post": post,
+        "comments": comments,
+    }
+
+    return render(request, "home/detail.html", context)
