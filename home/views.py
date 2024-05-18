5ed55244766a121aa203d535bb13dff6c2547545
iGagan <id4gsm@gmail.com>
2024-05-19 01:01:53 +0530

    Added content to home

--- home/views.py
+++ home/views.py
@@ -2,9 +2,5 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 
-def index(request):
-    return HttpResponse("You're at Home Page")
-
-
 def home_index(request):
     return render(request, "home/index.html")
