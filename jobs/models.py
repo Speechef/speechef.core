e12449cd2315eabd045f24004670ffb4277a5f83
iGagan <id4gsm@gmail.com>
2024-06-18 10:21:03 +0530

    Updating the design & model for Job module

--- jobs/models.py
+++ jobs/models.py
@@ -6,6 +6,12 @@ class Jobs(models.Model):
     title = models.CharField(max_length=100)
     description = models.TextField()
     date = models.DateTimeField(auto_now_add=True)
+    company = models.CharField(max_length=100, blank=True)
+    # logo = models.ImageField(upload_to='images/')
+    job_type = models.CharField(max_length=100 , blank=True)
+    location = models.CharField(max_length=100, blank=True)
+    url = models.URLField(blank=True)
+    
 
     def __str__(self):
         return self.title
